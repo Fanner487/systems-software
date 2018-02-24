9c1a72667f05a100bda46692d498371e558dde7e
Fanner487 <eamontang1@hotmail.com>
2018-02-24 12:52:58 +0000

    Commit

---
+++ assignment1/backuptest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <math.h>
+#include <string.h>
+
+
+int main(int argc, char **argv){
+
+	// char *command = argv[1];
+	// printf("\n%s\n", command);
+	char *arguments[] = {"cp", "a", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/backup", NULL};
+	execv("/bin/cp", arguments);
+
+	printf("Should not be here");
+
+	return 0;
+}
