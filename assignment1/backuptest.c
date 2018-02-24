6fba1f70080bcdcfff4e163315c9ced6fc91e8c9
Fanner487 <eamontang1@hotmail.com>
2018-02-24 13:02:19 +0000

    Commit

--- assignment1/backuptest.c
+++ assignment1/backuptest.c
@@ -11,8 +11,10 @@ int main(int argc, char **argv){
 
 	// char *command = argv[1];
 	// printf("\n%s\n", command);
-	char *arguments[] = {"cp", "a", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/backup"};
-	execvp("/bin/cp", arguments);
+	// char *arguments[] = {"cp", "a", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/backup"};
+	// execvp("/bin/cp", arguments);
+
+	execl("/bin/cp", "-a", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/backup", (char *)0);
 
 	printf("Should not be here");
 
