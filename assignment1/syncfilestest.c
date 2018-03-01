08a4aa9fe437b0ba244995337e6676d6688772f6
Fanner487 <eamontang1@hotmail.com>
2018-03-01 17:28:32 +0000

    sdfasdf

---
+++ assignment1/syncfilestest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <math.h>
+#include <string.h>
+
+
+void main(){
+
+	char *source = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/";
+	char *destination = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
+
+	execlp("rsync", "rsync", "-vr", source, destination, NULL);
+}
