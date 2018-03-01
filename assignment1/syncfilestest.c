302e2f108da10d468de809a19a8c055e466468cb
Fanner487 <eamontang1@hotmail.com>
2018-03-01 18:38:59 +0000

    sdfasdf

--- assignment1/syncfilestest.c
+++ assignment1/syncfilestest.c
@@ -13,22 +13,22 @@ void main(){
 	char *destination = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
 	char *command = "rsync -vr /home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/ /home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
 
-	// execlp("rsync", "rsync", "-vr", source, destination, NULL);
+	execlp("rsync", "rsync", "-vr", "--daemon", source, destination, NULL);
 
 
-	FILE *fp;
-	FILE *outputFile;
-	int status;
-	char string[] = "Wasssuppp mate!\n";
-	char readbuffer[1024];
+	// FILE *fp;
+	// FILE *outputFile;
+	// int status;
+	// char string[] = "Wasssuppp mate!\n";
+	// char readbuffer[1024];
 
-	fp = popen(command,"r");
-	outputFile = fopen("/home/eamon/Documents/software/systems-software/assignment1/synclog.txt", "a+");
+	// fp = popen(command,"r");
+	// outputFile = fopen("/home/eamon/Documents/software/systems-software/assignment1/synclog.txt", "a+");
 
-	while(fgets(readbuffer, 1024,fp) != NULL){
-		fprintf(outputFile, "%s", readbuffer);
-	}
+	// while(fgets(readbuffer, 1024,fp) != NULL){
+	// 	fprintf(outputFile, "%s", readbuffer);
+	// }
 
-	status = pclose(fp);
-	pclose(outputFile);
+	// status = pclose(fp);
+	// pclose(outputFile);
 }
