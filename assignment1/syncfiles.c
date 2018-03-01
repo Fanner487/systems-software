2d078b9c393d32825aa2586de842caa4ab230e11
Fanner487 <eamontang1@hotmail.com>
2018-03-01 18:56:08 +0000

    sdfasdf

--- assignment1/syncfiles.c
+++ assignment1/syncfiles.c
@@ -15,7 +15,7 @@ void syncFiles(){
 	char *destination = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
 	char *command = "rsync -r /home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/ /home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
 
-	execlp("rsync", "rsync", "-vr", source, destination, NULL);
+	execlp("rsync", "rsync", "-r", source, destination, NULL);
 
 	// system(command);
 
