0bb7244f42bff72ec5926d72f1299d737f3661e9
Fanner487 <eamontang1@hotmail.com>
2018-03-01 16:48:25 +0000

    sdfasdf

--- assignment1/rsynctest.c
+++ assignment1/rsynctest.c
@@ -11,19 +11,15 @@ int main(int argc, char **argv){
 
 	// char *command = argv[1];
 	// printf("\n%s\n", command);
-	char *arguments[] = {"rsync", "-vr", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live", 0};
-	execvp("rsync", arguments);
+	// char *arguments[] = {"rsync", "-vr", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live", 0};
+	// execvp("rsync", arguments);
+
+	char *source = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/";
+	char *destination = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
+
+	execlp("rsync", "rsync", "-vr", source, destinationWithDate, NULL);
 
 
-	// char *argumentscp[] = {"mv", "-v", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/intranet/*", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/", 0};
-	// execvp("/bin/cp", argumentscp);
-
-	// char *argumentsrm[] = {"rm", "-rf", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/intranet/", 0};
-	// execvp("/bin/rm", argumentsrm);
-
-	// execl("/bin/cp", "-r", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet", "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/backup", 0);
-
-	printf("Should not be here");
 
 	return 0;
 }
