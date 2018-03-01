aab686298589bbea4f544ce93d62545188bb3b9e
Fanner487 <eamontang1@hotmail.com>
2018-03-01 18:18:22 +0000

    sdfasdf

--- assignment1/syncfilestest.c
+++ assignment1/syncfilestest.c
@@ -6,11 +6,29 @@
 #include <math.h>
 #include <string.h>
 
-
+// #include ""
 void main(){
 
 	char *source = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/";
 	char *destination = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
+	char *command = "rsync -vr /home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/ /home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
 
-	execlp("rsync", "rsync", "-vr", source, destination, NULL);
+	// execlp("rsync", "rsync", "-vr", source, destination, NULL);
+
+
+	FILE *fp;
+	FILE *outputFile;
+	int status;
+	char string[] = "Wasssuppp mate!\n";
+	char readbuffer[1024];
+
+	fp = popen(command,"r");
+	outputFile = fopen("/home/eamon/Documents/software/systems-software/assignment1/synclog.txt", "a+");
+
+	while(fgets(readbuffer, 1024,fp) != NULL){
+		fprintf(outputFile, "%s", readbuffer);
+	}
+
+	status = pclose(fp);
+	pclose(outputFile);
 }
