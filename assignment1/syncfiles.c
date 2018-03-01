7cff28efad8300bdd827adb3cc2129a2345f17e1
Fanner487 <eamontang1@hotmail.com>
2018-03-01 18:44:40 +0000

    sdfasdf

--- assignment1/syncfiles.c
+++ assignment1/syncfiles.c
@@ -12,24 +12,49 @@ void syncFiles(){
 
 	char *source = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/";
 	char *destination = "/home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
-	char *command = "rsync -vr /home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/ /home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
+	char *command = "rsync -r /home/eamon/Documents/software/systems-software/assignment1/var/www/html/intranet/ /home/eamon/Documents/software/systems-software/assignment1/var/www/html/live/";
 
 	// execlp("rsync", "rsync", "-vr", source, destination, NULL);
 
+	system(command);
 
-	FILE *fp;
-	FILE *outputFile;
-	int status;
-	char string[] = "Wasssuppp mate!\n";
-	char readbuffer[1024];
 
-	fp = popen(command,"r");
-	outputFile = fopen("/home/eamon/Documents/software/systems-software/assignment1/synclog.txt", "a+");
+	// FILE *fp;
+	// FILE *outputFile;
+	// int status;
+	// char string[] = "Wasssuppp mate!\n";
+	// char readbuffer[1024];
 
-	while(fgets(readbuffer, 1024,fp) != NULL){
-		fprintf(outputFile, "%s", readbuffer);
-	}
+	// fp = popen(command,"r");
+	// outputFile = fopen("/home/eamon/Documents/software/systems-software/assignment1/synclog.txt", "a+");
 
-	status = pclose(fp);
-	pclose(outputFile);
+	// while(fgets(readbuffer, 1024,fp) != NULL){
+	// 	fprintf(outputFile, "%s", readbuffer);
+	// }
+
+	// status = pclose(fp);
+	// pclose(outputFile);
 }
+
+
+// //Update the website fron the intranet site
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <syslog.h>
+// #include <setjmp.h>
+
+// //#include "date.h"
+// #include "updatewebsite.h"
+
+// void updateWebsite(){
+//   char * update_exec_path = "rsync -r /Users/neilpelows/Documents/SystemsSoftware/assignment1/LIVEWEBSITE /var/www";
+
+//   if (system(update_exec_path) < 0){
+//     openlog("systemsoftware_log", LOG_PID|LOG_CONS, LOG_USER);
+//     syslog(LOG_INFO, "Failed to update live website: %s", strerror(errno));
+//     closelog();
+//   }
+// }
