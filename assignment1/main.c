303e0493c9bd6f3dbc25e51e232a6b12cf3feeea
Fanner487 <eamontang1@hotmail.com>
2018-03-01 15:14:04 +0000

    sdfasdf

---
+++ assignment1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <setjmp.h>
+
+
+
+void main (int argc, char **argv) {
+  printf("\nProgram is running.");
+
+  
+}
