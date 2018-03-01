42deee60a27762c15607dfbb062b8b103ac9e8d6
Fanner487 <eamontang1@hotmail.com>
2018-03-01 15:56:41 +0000

    sdfasdf

--- assignment1/date.c
+++ assignment1/date.c
@@ -3,11 +3,11 @@
 
 char* getCurrentDate(char * buffer){
 
-  time_t rawTime;
-  struct tm * timeinfo;
-  time (&rawTime);
-  timeinfo = localtime(&rawTime);
-  strftime(buffer, 80, "%d_%m_%Y", timeinfo);
+	time_t rawTime;
+	struct tm * timeinfo;
+	time (&rawTime);
+	timeinfo = localtime(&rawTime);
+	strftime(buffer, 80, "%d_%m_%Y", timeinfo);
 
-  return buffer;
+	return buffer;
 }
