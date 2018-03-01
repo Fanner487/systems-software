c2e54eebab92ea6efc9f2e5994106639d35e200b
Fanner487 <eamontang1@hotmail.com>
2018-03-01 17:08:30 +0000

    sdfasdf

--- assignment1/main.c
+++ assignment1/main.c
@@ -8,10 +8,41 @@
 #include <syslog.h>
 #include <setjmp.h>
 
-
+#include "backup.h"
 
 void main (int argc, char **argv) {
   printf("\nProgram is running.");
 
-  
+  int pid = fork();
+
+  // Parent
+	if(pid > 0){
+
+		printf("Parent process");
+		sleep(10);
+		exit(EXIT_SUCCESS);
+	}
+	else if(pid == 0){
+
+		printf("Child process");
+
+		if(setsid() < 0 ){
+			exit(EXIT_FAILURE);
+		}
+
+		umask(0);
+
+		if(chdir("/") < 0){
+			exit(EXIT_FAILURE);
+		}
+
+		int x;
+
+		for(x = sysconf(_SC_OPEN_MAX); x >= 0; x--){
+			close(x);
+		}
+
+		backup();
+	}
+
 }
